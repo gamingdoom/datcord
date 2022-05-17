2170279d70352afb9f47ac0d93b1aee093356909
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-05-17 12:30:59 -0700

    update build script

--- buildDatcord.sh
+++ buildDatcord.sh
@@ -4,9 +4,15 @@ mkdir build
 if [ -z "$(ls build)" ]; then
   git clone --recurse-submodules https://github.com/mozilla/gecko-dev.git build
   build/mach create-mach-environment
+else 
+  rm -rf build
+  git clone --recurse-submodules https://github.com/mozilla/gecko-dev.git build
+  build/mach create-mach-environment
 fi
 cd build
-cp ../changed/* .
+mkdir .mozbuild
+export MOZBUILD_STATE_PATH=".mozbuild"
+cp -r ../changed/* .
 patch . ../mozilla_dirsFromLibreWolf.patch
 ./mach build
 ./mach package
