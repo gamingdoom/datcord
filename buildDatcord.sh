29c78420993fd78a830aaee277294345ada5eeac
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-05-17 15:32:56 -0700

    Update buildDatcord.sh

--- buildDatcord.sh
+++ buildDatcord.sh
@@ -6,7 +6,7 @@ mkdir build
 #fi
 cd build
 curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
-python3 bootstrap.py
+python3 bootstrap.py --vcs=git
 ./mach create-mach-environment
 cp -r ../changed/* .
 patch . ../mozilla_dirsFromLibreWolf.patch
