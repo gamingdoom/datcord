bfc0b7460063acc90664eceecf4b490a9e0b394f
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-05-18 16:49:57 -0700

    Update buildDatcord.sh

--- buildDatcord.sh
+++ buildDatcord.sh
@@ -7,6 +7,7 @@
 #cd build
 curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
 python3 bootstrap.py --vcs=git
+cd mozilla-unified
 mozbuild=~/.mozbuild
 export PATH="$PATH:$mozbuild/git-cinnabar"
 ./mach bootstrap
