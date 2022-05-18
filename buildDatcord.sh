1deb96c0dff69340ceb1a645c4672b5b61844c15
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-05-18 16:47:32 -0700

    Update buildDatcord.sh

--- buildDatcord.sh
+++ buildDatcord.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-mkdir build
-if [ -z "$(ls build)" ]; then
-  git clone --recurse-submodules -b release https://github.com/mozilla/gecko-dev.git build
-fi
-cd build
-#curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
-#python3 bootstrap.py --vcs=git
-./mach bootstrap
-./mach create-mach-environment
-cp -r ../changed/* .
-patch . ../mozilla_dirsFromLibreWolf.patch
-./mach configure
+#mkdir build
+#if [ -z "$(ls build)" ]; then
+# git clone --recurse-submodules -b release https://github.com/mozilla/gecko-dev.git build
+#fi
+#cd build
+curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
+python3 bootstrap.py --vcs=git
 mozbuild=~/.mozbuild
 export PATH="$PATH:$mozbuild/git-cinnabar"
+./mach bootstrap
+cp -r ../changed/* .
+patch -p1 ../mozilla_dirsFromLibreWolf.patch
+./mach configure
 ./mach build
 ./mach package
