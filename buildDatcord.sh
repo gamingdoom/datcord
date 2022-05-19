f8388caf5c2f35dc28a84592833c8d03193691a1
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-05-18 17:13:37 -0700

    Update buildDatcord.sh

--- buildDatcord.sh
+++ buildDatcord.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#mkdir build
-#if [ -z "$(ls build)" ]; then
-# git clone --recurse-submodules -b release https://github.com/mozilla/gecko-dev.git build
-#fi
-#cd build
-curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
-python3 bootstrap.py --vcs=git
+mkdir mozilla-unified
+if [ -z "$(ls mozilla-unified)" ]; then
+  rm mozilla-unified
+  curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
+  python3 bootstrap.py --vcs=git
+fi
 cd mozilla-unified
 mozbuild=~/.mozbuild
 export PATH="$PATH:$mozbuild/git-cinnabar"
-./mach bootstrap
 cp -r ../changed/* .
 patch -p1 ../mozilla_dirsFromLibreWolf.patch
 ./mach configure
