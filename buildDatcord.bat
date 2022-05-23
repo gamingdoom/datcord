cc6e34abb3be2342e019cfd2fd0a8754fb7e55ab
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-05-22 20:21:16 -0700

    Update buildDatcord.bat

--- buildDatcord.bat
+++ buildDatcord.bat
@@ -1,7 +1,7 @@
 curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py --output bootstrap.py
 python3 bootstrap.py --no-interactive
 rm bootstrap.py
-copy /Y ..\changed\* .\mozilla-unified\
+xcopy changed\*.* .\mozilla-unified /S
 cd mozilla-unified
 ./mach build
 ./mach package
