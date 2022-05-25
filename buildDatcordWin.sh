82cb93a8cf85e4c11e331fe2941b41fa16476eb2
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-05-25 16:36:59 -0700

    Update and rename buildDatcord.bat to buildDatcordWin.sh

--- buildDatcordWin.sh
+++ buildDatcordWin.sh
@@ -1,6 +1,8 @@
+# Run with MozillaBuild
+basedir=$(dirname "$0")
 curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py --output bootstrap.py
 python3 bootstrap.py --no-interactive
-cp -r changed/* mozilla-unified/
+cp -r $basedir/changed/* mozilla-unified/
 cd mozilla-unified
 ./mach build
 ./mach package
