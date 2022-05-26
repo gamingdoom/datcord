c2340b27feffee2398a640bcd29ac2c79635050c
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-05-26 12:10:22 -0700

    Update buildDatcordWin.sh

--- buildDatcordWin.sh
+++ buildDatcordWin.sh
@@ -4,7 +4,7 @@ curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/
 python3 bootstrap.py --no-interactive
 cp -r $basedir/changed/* mozilla-unified/
 cd mozilla-unified
-cat "ac_add_options --with-app-name=datcord" >> .mozconfig
-cat "ac_add_options --with-branding=browser/branding/unofficial" >> .mozconfig
+cat "ac_add_options --with-app-name=datcord" >> mozconfig
+cat "ac_add_options --with-branding=browser/branding/unofficial" >> mozconfig
 ./mach build
 ./mach package
