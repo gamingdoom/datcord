4b526f5a522eca1d96a62f47af6f691d7a8eac56
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-10-10 16:43:34 -0700

    check if mozilla-unified already exists

--- buildDatcordWin.sh
+++ buildDatcordWin.sh
@@ -1,7 +1,9 @@
 # Run with MozillaBuild
 basedir=$(dirname "$0")
-curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py --output bootstrap.py
-python3 bootstrap.py --no-interactive
+if [[! -d mozilla-unified]]; then
+  curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py --output bootstrap.py
+  python3 bootstrap.py --no-interactive
+fi
 cp -rf $basedir/changed/* mozilla-unified/
 # It is using nightly branding no matter what so we replace the nightly stuff with our stuff
 cp -rf mozilla-unified/browser/branding/unofficial/* mozilla-unified/browser/branding/nightly/*
