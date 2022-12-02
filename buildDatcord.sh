c7076f320fc1ab193e1ed0ec5438e3b7d9887567
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-12-01 18:48:45 -0800

    Update buildDatcord.sh

--- buildDatcord.sh
+++ buildDatcord.sh
@@ -7,7 +7,7 @@ mkdir mozilla-unified
 if [ -z "$(ls mozilla-unified)" ]; then
   rm mozilla-unified
   curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
-  python3 bootstrap.py --vcs=git --no-interactive --choice 1
+  python3 bootstrap.py --vcs=git --no-interactive --application-choice=browser_artifact_mode
   $mozbuild/git-cinnabar/git-cinnabar install
 fi
 cd mozilla-unified
