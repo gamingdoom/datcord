91703245b32a18cd54df667cfb321cfa35aa4ee7
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2023-01-20 12:48:22 -0800

    remove dir

--- buildDatcord.sh
+++ buildDatcord.sh
@@ -5,7 +5,7 @@ export PATH="$PATH:$mozbuild/git-cinnabar"
 echo $PATH
 mkdir mozilla-unified
 if [ -z "$(ls mozilla-unified)" ]; then
-  rm mozilla-unified
+  rm -r mozilla-unified
   curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
   python3 bootstrap.py --vcs=git --no-interactive --application-choice=browser_artifact_mode
   $mozbuild/git-cinnabar/git-cinnabar install
