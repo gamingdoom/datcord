f00787e2733820d35becf17d692c5dba94ac8911
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2023-01-23 15:59:28 -0800

    fix build and fix ui elements

--- buildDatcord.sh
+++ buildDatcord.sh
@@ -2,14 +2,27 @@
 
 mozbuild=~/.mozbuild
 export PATH="$PATH:$mozbuild/git-cinnabar"
-echo $PATH
-mkdir mozilla-unified
-if [ -n "$(ls mozilla-unified)" ]; then
-  rm -r mozilla-unified
+datcordDir=$PWD
+
+if [ ! -d $mozbuild ]; then
+  mkdir $mozbuild
+fi
+
+if [ ! -d mozilla-unified ]; then
+  mkdir mozilla-unified
   curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
   python3 bootstrap.py --vcs=git --no-interactive --application-choice=browser_artifact_mode
-  $mozbuild/git-cinnabar/git-cinnabar install
 fi
+
+if [ ! -d $mozbuild/git-cinnabar ]; then
+  git clone https://github.com/glandium/git-cinnabar.git $mozbuild/git-cinnabar
+  cd $mozbuild/git-cinnabar
+  make
+  cd $datcordDir
+
+  $mozbuild/git-cinnabar/git-cinnabar install
+fi	
+
 cd mozilla-unified
 cp -r ../changed/* .
 patch -p1 ../mozilla_dirsFromLibreWolf.patch
