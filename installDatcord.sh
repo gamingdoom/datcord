e240fe45d905fb3c6a4bd6beddf85ab85821b8da
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2023-01-31 18:18:24 -0800

    update install script

--- installDatcord.sh
+++ installDatcord.sh
@@ -4,8 +4,9 @@ rm datcord-linux-x86_64.tar.bz2
 wget https://github.com/gamingdoom/datcord/releases/latest/download/datcord-linux-x86_64.tar.bz2
 mkdir datcord-bin
 tar -xvf datcord-linux-x86_64.tar.bz2 -C datcord-bin/
-sudo mkdir /usr/bin/datcord.d
-sudo cp -r datcord-bin/* /usr/bin/datcord.d
+chmod +x datcord-bin/launch-datcord
+chmod +x datcord-bin/open-in-default-browser
+sudo mkdir /usr/lib/datcord
+sudo cp -r datcord-bin/* /usr/lib/datcord
 chmod +x /usr/bin/launch-datcord
-sudo ln -s /usr/bin/datcord.d/launch-datcord /usr/bin/datcord
-sudo cp datcord-bin/open-in-default-browser/open-in-default-browser /usr/bin/open-in-default-browser && chmod +x /usr/bin/open-in-default-browser
+sudo ln -s /usr/lib/datcord/launch-datcord /usr/bin/datcord
